deb5510a33a6da5d37912282f250063693628c9f
carp533 <olexyz@gmx.net>
2023-06-13 10:26:54 +0200

    Labor 13.6.

--- labor/aufgaben_01062023_generics_test.go
+++ labor/aufgaben_01062023_generics_test.go
@@ -6,7 +6,7 @@ package main
 // der Liste list
 // TODO: Ändere den Datentyp, sodass die Funktion eine Liste mit einem generischen
 // Typ verarbeiten kann (type constraint any)
-func FindCond(list []int, f func(int) bool) []int {
+func FindCond[T any](list []T, f func(T) bool) []int {
 	res := make([]int, 0)
 	for i, v := range list {
 		if f(v) {
@@ -24,24 +24,27 @@ func Greater50(v int) bool {
 // die größer als 50 sind.
 // TODO: programmiere die Funktion und nutze Greater50
 func FindIfGreater50(list []int) []int {
-	return nil
-	//TODO
+	return FindCond(list, Greater50)
 }
 
 // FindIfOdd erwartet eine Liste und liefert die Position aller Elemente,
 // die ungerade sind.
 // TODO: programmiere die Funktion und nutze eine anonyme Funktion
 func FindIfOdd(list []int) []int {
-	return nil
-	//TODO
+	isOdd := func(v int) bool {
+		return v%2 != 0
+	}
+	return FindCond(list, isOdd)
 }
 
 // FindIfBetween erwartet eine Liste und zwei Zahlen a,b.
 // Sie liefert alle Position, die zwischen a und b liegen.
 //TODO: programmiere die Funktion, mit Hilfe einer anonymen Funktion
 func FindIfBetween(list []int, a, b int) []int {
-	return nil
-	//TODO
+	return FindCond(
+		list,
+		func(v int) bool { return v > a && v < b },
+	)
 }
 
 //TODO: Programmiere eine Example Funktion, die alle obigen Funktionen testet
